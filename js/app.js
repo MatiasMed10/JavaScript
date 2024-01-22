7d7e19cd4352ae05d2f6b672119d9872e9185ed8
matiasmed <medinamatias63c1@gmail.com>
2024-01-22 13:39:25 -0300

    Comenzamos con los primeros conceptos de js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
 //Aqui vamos a comnzar con javascript
+console.log('Hola, Mundo!!');
+
+//Tipo de datos
+
+/*String: Cadena de caracteres, Se lo respresenta por que esta 
+contenido entre comillas simples o dobles*/
+//Number: Todos los numeros
+//Booleanos: Logicos solo toman 2 valores True o False
+
+//Array:
+//Object:
+//Empty: Null, nada.
+
+//Variables: (var - let - const)
+
+//var deprecada --> no se usa
+//crear o declarar una variable
+
+//sintaxis:
+/*
+tipo de variable [nombre de la variable (camelCase /
+nombre-de-la-variable / nombre_de_la_variable)] = [string o 
+number o boolean o arrayu o object];
+*/
+
+let mensaje = "Esto es un mensaje";
+console.log(mensaje);
+document.write('<br>' + mensaje);
+const token = 'token';
+//console.log('let mensaje-->',mensaje);
+console.log('const token-->',token);
+//Cambiar valor de una variale
+
+mensaje = 'Esto es otro mensaje distinto';
+console.log(mensaje);
+document.write('<br>' + mensaje);
+
+mensaje = 50;   
+console.log(mensaje);
+document.write('<br>' + mensaje);
+
+//Esto es un error --> token = 'otro token'; (No se puede cambir el valor de una constante)
+
+//imprimir datos en el documento html
+
+//document.write("Esto es un document.write");
+
